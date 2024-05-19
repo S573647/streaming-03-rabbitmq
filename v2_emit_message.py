2a1d963132d20fc16304003981735f1c3fc0cb36
S573647 <S573647@nwmissouri.edu>
2024-05-19 18:24:10 -0500

    commit the changes

--- v2_emit_message.py
+++ v2_emit_message.py
@@ -39,6 +39,7 @@ def send_message(host: str, queue_name: str, message: str):
     """
 
     try:
+        conn = None;
         # create a blocking connection to the RabbitMQ server
         conn = pika.BlockingConnection(pika.ConnectionParameters(host))
 
@@ -66,4 +67,4 @@ def send_message(host: str, queue_name: str, message: str):
 # If this is the script we are running, then call some functions and execute code!
 # ---------------------------------------------------------------------------
 if __name__ == "__main__":
-    send_message("llllocalhost", "hello", "Hello World!")
+    send_message("localhost", "hello", "Hello World!")
